6d497cdb45297803864e2c58455c4163c36f1f3b
Siddhart <Siddhart_7@live.nl>
2023-05-14 22:11:48 +0200

    some small changes

--- src/test/java/MCDC.java
+++ src/test/java/MCDC.java
@@ -4,22 +4,22 @@ import org.junit.Assert;
 import org.junit.Test;
 import java.time.LocalDate;
 
-public class ProjectTest {
+public class MCDC {
     @Test
     public void MCDCTest() {
         Main main = new Main();
 
         //Modified Condition/Decision Coverage
-        //eind > start && klant && medewerker
+        //start < eind && klant && medewerker
         Assert.assertTrue(Checks.checkProject("2023-01-01", "2023-02-02", main.klanten, 1, main.medewerkers, 4));
 
         //start > end && klant && medewerker
         Assert.assertFalse(Checks.checkProject("2023-02-02", "2023-01-01", main.klanten, 1, main.medewerkers, 4));
 
-        //start > end && klant && !medewerker
-        Assert.assertFalse(Checks.checkProject("2023-02-02", "2023-01-01", main.klanten, 1, main.medewerkers, 400));
+        //start < end && !klant && medewerker
+        Assert.assertFalse(Checks.checkProject("2023-01-01", "2023-02-02", main.klanten, 100, main.medewerkers, 4));
 
-        //start > end && !klant && medewerker
-        Assert.assertFalse(Checks.checkProject("2023-02-02", "2023-01-01", main.klanten, 1, main.medewerkers, 400));
+        //start < end && klant && !medewerker
+        Assert.assertFalse(Checks.checkProject("2023-01-01", "2023-02-02", main.klanten, 1, main.medewerkers, 400));
     }
 }
--- src/test/java/PairwiseTests.java
+++ src/test/java/PairwiseTests.java
@@ -8,22 +8,22 @@ public class PairwiseTests {
     @Test
     public void PairwiseTests() {
         Main main  = new Main();
-        // Geen project - Kleiner dan 0, geldige datum, bestaat, bestaat
+        //Kleiner dan 0, geldige datum, bestaat, bestaat
         Assert.assertFalse(Checks.projectCheck(-1, "2023-05-13", "2023-05-20", 4, main.medewerkers, 1, main.klanten));
 
-        // Geen project - Kleiner dan 0, ongeldige datum, bestaat niet, bestaat niet
-        Assert.assertFalse(Checks.projectCheck(-1, "2023-05-30", "2023-05-20", 4, main.medewerkers, 1, main.klanten));
+        //Kleiner dan 0, ongeldige datum, bestaat niet, bestaat niet
+        Assert.assertFalse(Checks.projectCheck(-1, "2023-05-30", "2023-05-20", 400, main.medewerkers, 100, main.klanten));
 
-        // Geen project - Gelijk aan 0, geldige datum, bestaat niet, bestaat niet
-        Assert.assertFalse(Checks.projectCheck(0, "2023-05-13", "2023-05-20", 10, main.medewerkers, 5, main.klanten));
+        //Gelijk aan 0, geldige datum, bestaat niet, bestaat niet
+        Assert.assertFalse(Checks.projectCheck(0, "2023-05-13", "2023-05-20", 400, main.medewerkers, 100, main.klanten));
 
-        // Geen project - Gelijk aan 0, ongeldige datum, bestaat, bestaat
+        //Gelijk aan 0, ongeldige datum, bestaat, bestaat
         Assert.assertFalse(Checks.projectCheck(0, "2023-05-30", "2023-05-20", 4, main.medewerkers, 1, main.klanten));
 
-        // Project aanmaken - Groter dan 0, geldige datum, bestaat, bestaat
+        //Groter dan 0, geldige datum, bestaat, bestaat
         Assert.assertTrue(Checks.projectCheck(1000, "2023-05-13", "2023-05-20", 4, main.medewerkers, 1, main.klanten));
 
-        // Geen project - Groter dan 0, ongeldige datum, bestaat niet, bestaat niet
-        Assert.assertFalse(Checks.projectCheck(1000, "2023-05-30", "2023-05-20", 10, main.medewerkers, 5, main.klanten));
+        //Groter dan 0, ongeldige datum, bestaat niet, bestaat niet
+        Assert.assertFalse(Checks.projectCheck(1000, "2023-05-30", "2023-05-20", 400, main.medewerkers, 100, main.klanten));
     }
 }
